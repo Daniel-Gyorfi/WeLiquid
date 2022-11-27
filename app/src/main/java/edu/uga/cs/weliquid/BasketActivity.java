0900a96b4162ec11cb233bd6a5720b898d2bf0e3
Daniel Gyorfi <dg32081@uga.edu>
2022-11-27 12:07:55 -0500

    Added shopping basket button to Item Management

---
+++ app/src/main/java/edu/uga/cs/weliquid/BasketActivity.java
@@ -0,0 +1,17 @@
+package edu.uga.cs.weliquid;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+/**
+ * The Basket Screen is shown here,
+ */
+public class BasketActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_basket);
+    }
+}
